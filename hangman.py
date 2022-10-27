8439bea205ad7a7a3285c77e34684397c4b97fed
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2022-10-27 17:47:15 +0100

    clean code and add show idden word

--- hangman.py
+++ hangman.py
@@ -1,5 +1,5 @@
 import os
-os.system('clear')
+# os.system('clear')
 
 hangman_steps = ['''
   +---+
@@ -59,35 +59,33 @@ hangman_steps = ['''
       |
 =========''']
 
-
-
-
+# create secret word dictionary 
 word_dict = {}
 word = 'prova'
 for i in range(len(word)):
     word_dict[i] = ['_', word[i]]
+'''
+result:
+0 ['_', 'p']
+1 ['_', 'r']
+2 ['_', 'o']
+3 ['_', 'v']
+4 ['_', 'a']
+'''
 
-for i, j in word_dict.items():
-  print(i, j)
-quit()
+# show the hidden word
+for i in word_dict:
+    print(word_dict[0][0], end='')
 
-
-
-
-
-
-
+# vars setting
 step_num = 0
 print(hangman_steps[step_num])
-word_hidden = '*' * len(word)
-print(word_hidden)
 
+# core game
 while step_num < 7:
     guess = input('what letter? > ')
-    os.system('clear')
+    # os.system('clear')
     if not guess in word:
         step_num += 1
-
     print(hangman_steps[step_num])
-    print(word_hidden)
     print(step_num)
