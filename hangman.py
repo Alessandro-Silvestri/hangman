924f6944d42683ff573195f848174f73e0961a92
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2022-10-27 16:50:47 +0100

    created word_dict, it will be dinamically generated according the secret word

--- hangman.py
+++ hangman.py
@@ -1,3 +1,6 @@
+import os
+os.system('clear')
+
 hangman_steps = ['''
   +---+
       |
@@ -56,14 +59,35 @@ hangman_steps = ['''
       |
 =========''']
 
-# workin core script 
+
+
+
+word_dict = {}
+word = 'prova'
+for i in range(len(word)):
+    word_dict[i] = ['_', word[i]]
+
+for i, j in word_dict.items():
+  print(i, j)
+quit()
+
+
+
+
+
+
+
 step_num = 0
 print(hangman_steps[step_num])
+word_hidden = '*' * len(word)
+print(word_hidden)
 
-word = 'prova'
-guess = input('what letter? > ')
+while step_num < 7:
+    guess = input('what letter? > ')
+    os.system('clear')
+    if not guess in word:
+        step_num += 1
 
-if not guess in word:
-    step_num += 1
-
-print(hangman_steps[step_num])
+    print(hangman_steps[step_num])
+    print(word_hidden)
+    print(step_num)
