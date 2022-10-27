3b8b2564b617e7b199043768b7f78648315ca909
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2022-10-27 18:32:45 +0100

    convert all the code in a class

--- hangman.py
+++ hangman.py
@@ -1,71 +1,100 @@
 import os
 # os.system('clear')
 
-hangman_steps = ['''
-  +---+
-      |
-      |
-      |
-      |
-      |
-=========''','''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''']
 
-# create secret word dictionary 
-word_dict = {}
-word = 'prova'
-for i in range(len(word)):
-    word_dict[i] = ['_', word[i]]
+class Hangman:
+    def __init__(self):
+        # create secret word dictionary 
+        self.word_dict = {}
+        self.step_num = 0
+        self.word = 'prova' 
+        for i in range(len(self.word)):
+            self.word_dict[i] = ['_', self.word[i]]
+ 
+    def get_hidden_word(self):
+        '''show the hiddemn word like this: ______'''
+        # show the hidden word
+        for i in self.word_dict:
+            print(self.word_dict[0][0], end='')
+
+    def core_game(self):
+        '''user interaction'''
+        print(self.death()[self.step_num])
+
+        # core game
+        while self.step_num < 7:
+            self.guess = input('what letter? > ')
+            # os.system('clear')
+            if not self.guess in self.word:
+                self.step_num += 1
+            print(self.death()[self.step_num])
+            print(self.step_num)
+        quit()
+
+    def death(self):
+        '''steps to death'''
+        self.hangman_steps = ['''
+            +---+
+                |
+                |
+                |
+                |
+                |
+            =========''','''
+            +---+
+            |   |
+                |
+                |
+                |
+                |
+            =========''', '''
+            +---+
+            |   |
+            O   |
+                |
+                |
+                |
+            =========''', '''
+            +---+
+            |   |
+            O   |
+            |   |
+                |
+                |
+            =========''', '''
+            +---+
+            |   |
+            O   |
+           /|   |
+                |
+                |
+            =========''', '''
+            +---+
+            |   |
+            O   |
+           /|\  |
+                |
+                |
+            =========''', '''
+            +---+
+            |   |
+            O   |
+           /|\  |
+           /    |
+                |
+            =========''', '''
+            +---+
+            |   |
+            O   |
+           /|\  |
+           / \  |
+                |
+            =========''']
+        return self.hangman_steps
+
+
 '''
-result:
+self.word_dict (result example)
 0 ['_', 'p']
 1 ['_', 'r']
 2 ['_', 'o']
@@ -73,19 +102,5 @@ result:
 4 ['_', 'a']
 '''
 
-# show the hidden word
-for i in word_dict:
-    print(word_dict[0][0], end='')
-
-# vars setting
-step_num = 0
-print(hangman_steps[step_num])
-
-# core game
-while step_num < 7:
-    guess = input('what letter? > ')
-    # os.system('clear')
-    if not guess in word:
-        step_num += 1
-    print(hangman_steps[step_num])
-    print(step_num)
+play = Hangman()
+print(play.core_game())
